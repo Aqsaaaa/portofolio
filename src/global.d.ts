e09e13abff9d7006bf6e9d21a634c6e03c4351c3
Aqsaaaa <cendekiaqsa@gmail.com>
2025-11-25 04:46:12 +0700

    solve

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,18 +1,10 @@
-export { };
+// meshline.d.ts
+import { Object3DNode } from '@react-three/fiber';
+import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
 
-declare module '*.glb';
-declare module '*.png';
-
-declare module 'meshline' {
-  export const MeshLineGeometry: any;
-  export const MeshLineMaterial: any;
-}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      meshLineGeometry: any;
-      meshLineMaterial: any;
-    }
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+    meshLineMaterial: Object3DNode<MeshLineMaterial, typeof MeshLineMaterial>;
   }
 }
